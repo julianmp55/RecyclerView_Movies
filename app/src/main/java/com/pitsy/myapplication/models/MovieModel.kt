6fa29e30f4812381949d01116d015a010739b84b
Julian <julianmp0@gmail.com>
2020-05-31 11:07:30 -0500

    Initial commit

---
+++ app/src/main/java/com/pitsy/myapplication/models/MovieModel.kt
@@ -0,0 +1,10 @@
+package com.pitsy.myapplication.models
+
+class MovieModel(
+    val nameMovie: String="",
+    val nameActor: String="",
+    val movieCategory: String="",
+    val idMovie: String=""
+) {
+
+}
